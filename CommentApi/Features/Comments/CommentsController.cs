6ab124438d2dfff57e410bc68b93029d2c9044dc
dabananda <dabananda.dev@gmail.com>
2026-07-19 10:49:41 +0600

    Added endpoints to the controller

--- CommentApi/Features/Comments/CommentsController.cs
+++ CommentApi/Features/Comments/CommentsController.cs
@@ -1,7 +1,10 @@
 ﻿using CommentApi.Common.Abstraction;
 using CommentApi.Controllers;
 using CommentApi.Features.Comments.Create;
+using CommentApi.Features.Comments.Delete;
 using CommentApi.Features.Comments.GetAll;
+using CommentApi.Features.Comments.GetById;
+using CommentApi.Features.Comments.Update;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommentApi.Features.Comments
@@ -19,6 +22,27 @@ namespace CommentApi.Features.Comments
         public async Task<IActionResult> GetComments([FromQuery] GetAllQuery query)
         {
             var result = await sender.Send(query);
+            return Handle(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCommentById([FromRoute] Guid id)
+        {
+            var result = await sender.Send(new GetByIdQuery(id));
+            return Handle(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateComment([FromBody] UpdateCommand command)
+        {
+            var result = await sender.Send(command);
+            return Handle(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> DeleteComment([FromRoute] Guid id)
+        {
+            var result = await sender.Send(new DeleteCommand(id));
             return Handle(result);
         }
     }
