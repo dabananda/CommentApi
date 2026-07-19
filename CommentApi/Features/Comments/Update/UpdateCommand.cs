637ec0ee0f00c5440aaa1dfabb9bc6a4053ac96b
dabananda <dabananda.dev@gmail.com>
2026-07-19 10:37:25 +0600

    feat: delete command added

---
+++ CommentApi/Features/Comments/Update/UpdateCommand.cs
@@ -0,0 +1,13 @@
+﻿using CommentApi.Common;
+using CommentApi.Common.Abstraction;
+
+namespace CommentApi.Features.Comments.Update
+{
+    public record UpdateCommand(
+        Guid Id,
+        string PostId,
+        string AuthorName,
+        string? AuthorEmail,
+        string Content,
+        string? ParentCommentId) : IRequest<Result>;
+}
---
+++ CommentApi/Features/Comments/Update/UpdateCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using CommentApi.Common;
+using CommentApi.Common.Abstraction;
+using CommentApi.Repositories;
+
+namespace CommentApi.Features.Comments.Update
+{
+    public class UpdateCommandHandler(ICommentRepository commentRepository) : IRequestHandler<UpdateCommand, Result>
+    {
+        public async Task<Result> Handle(UpdateCommand request, CancellationToken cancellationToken)
+        {
+            var comment = await commentRepository.GetCommentByIdAsync(request.Id, cancellationToken);
+
+            if (comment is null)
+                return Result.Failure("No comment found");
+
+            comment.AuthorName = request.AuthorName;
+            comment.AuthorEmail = request.AuthorEmail;
+            comment.Content = request.Content;
+
+            await commentRepository.UpdateCommentAsync(comment, cancellationToken);
+
+            return Result.Success("Comment updated successfully");
+        }
+    }
+}
