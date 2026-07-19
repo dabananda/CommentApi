c97b7644ed2ce1d5c0cf946062d0300233c693a4
dabananda <dabananda.dev@gmail.com>
2026-07-19 10:23:00 +0600

    feat: added get comment by id query

---
+++ CommentApi/Features/Comments/GetById/GetByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using CommentApi.Common;
+using CommentApi.Common.Abstraction;
+
+namespace CommentApi.Features.Comments.GetById
+{
+    public record GetByIdQuery(Guid Id) : IRequest<Result<CommentDto>>;
+}
---
+++ CommentApi/Features/Comments/GetById/GetByIdQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using CommentApi.Common;
+using CommentApi.Common.Abstraction;
+using CommentApi.Repositories;
+
+namespace CommentApi.Features.Comments.GetById
+{
+    public class GetByIdQueryHandler(ICommentRepository commentRepository) : IRequestHandler<GetByIdQuery, Result<CommentDto>>
+    {
+        public async Task<Result<CommentDto>> Handle(GetByIdQuery request, CancellationToken cancellationToken)
+        {
+            var comment = await commentRepository.GetCommentByIdAsync(request.Id, cancellationToken);
+
+            if (comment is null)
+                return Result<CommentDto>.Failure("No comment found");
+
+            return Result<CommentDto>.Success(comment.ToDto());
+        }
+    }
+}
